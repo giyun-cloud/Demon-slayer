fccba0cb56d1292fcec3f5b850eea60c603e8a73
giyun-cloud <nangiyoon@gmail.com>
2021-04-04 20:28:14 +0900

    npm추가 및 js조금 만듬

---
+++ main.js
@@ -0,0 +1,7 @@
+// const pillarEls = document.querySelectorAll('.pillar')
+// const descriptionH4El = document.querySelector('.description h4')
+// pillarEls.forEach((el,i) => {
+//   el.addEventListener('hover',() => {
+
+//   })
+// })
